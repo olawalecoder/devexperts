e969b6faec5da72cdb797fea999edb9a5240d244
olawalecoder <bolawale81@gmail.com>
2024-11-09 17:25:28 +0100

    Add light/dark theme and shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import React from "react";
+import ThemeProvider from "@/context/Theme";
 
 const inter = localFont({
   src: "./fonts/InterVf.ttf",
@@ -29,11 +30,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable}} antialiased`}
       >
+      <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+      >
         {children}
+      </ThemeProvider>
+
       </body>
     </html>
   );
