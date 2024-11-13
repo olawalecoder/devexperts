6cf566115e70cd3cafbe546c0fff65cd2a8a6880
olawalecoder <bolawale81@gmail.com>
2024-11-13 14:19:15 +0100

    Add sign-in and sign-up form

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,21 @@
+"use client"
+
 import React from 'react'
+
+import AuthForm from "@/components/forms/AuthForm";
+import { SignUpSchema} from "@/lib/validations";
 
 const SignUp = () => {
     return (
-        <div>SignUp</div>
+        <>
+            <AuthForm
+                formType="SIGN_UP"
+                schema={SignUpSchema}
+                defaultValues={{name: "", username: "", email: "", password: ""}}
+                onSubmit={(data) => Promise.resolve({success: true, data})}
+            />
+
+        </>
     )
 }
 export default SignUp
