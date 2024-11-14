b3a050668ada8868c89839c88a78f28d6b8962f5
olawalecoder <bolawale81@gmail.com>
2024-11-14 23:44:51 +0100

    Display and route NavLinks

--- components/navigation/navbar/NavLinks.tsx
+++ components/navigation/navbar/NavLinks.tsx
@@ -1,10 +1,48 @@
+"use client"
+
 import React from 'react'
+import { usePathname } from "next/navigation";
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+import {cn} from "@/lib/utils";
 
+const NavLinks = ({ isMobileNav = false }: { isMobileNav?: boolean }) => {
+    const pathname = usePathname();
 
+    const userId = 1;
 
-const NavLinks = ({isMobileNav = false} : {isMobileNav?: boolean}) => {
     return (
-        <div>NavLinks</div>
-    )
+        <>
+            {sidebarLinks.map((item) => {
+
+                const isActive = (pathname.includes(item.route) && item.route.length > 1) || pathname === item.route;
+
+                if (item.route === "/profile") {
+                    if (userId) item.route = `${item.route}/${userId}`
+                    else return null;
+                }
+
+                return (
+                    <Link href={item.route} key={item.label} className={cn(
+                        isActive
+                            ? "primary-gradient rounded-lg text-light-900"
+                            : "text-dark300_light900",
+                        "flex items-center justify-start gap-4 bg-transparent p-4"
+                    )}>
+                        <Image
+                            src={item.imgURL}
+                            alt={item.label}
+                            width={20}
+                            height={20}
+                            className={cn({ "invert-colors": !isActive })}
+                        />
+                        <p>{item.label}</p>
+                    </Link>
+                );
+            })}
+        </>
+    );
 }
-export default NavLinks
+
+export default NavLinks;
