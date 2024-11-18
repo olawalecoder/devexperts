d8d7a47185d46eccb6358cf9ad414012edea9fbf
olawalecoder <bolawale81@gmail.com>
2024-11-18 21:12:03 +0100

    Homepage layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,31 @@
-import {auth, signOut} from "@/auth";
-import ROUTES from "@/constants/routes";
+import Link from "next/link";
+
 import {Button} from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
 
 const Home = async () => {
-    const session = await auth()
-    console.log(session)
   return (
       <>
-          <h1 className="h1-bold">
-              Welcome to DevExperts
-          </h1>
-          <h1 className="text-5xl font-space-grotesk">
-              Welcome to DevExperts
-          </h1>
+          <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+              <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+              <Button
+                  className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+                  asChild
+              >
+                  <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+              </Button>
+          </section>
+          <section className="mt-11">Local Serach
+          </section>
+          <div> HomeFilter</div>
+          <div className="mt-10 flex w-full flex-col gap-6">
+              <p>Question Card 1</p>
+              <p>Question Card 1</p>
+              <p>Question Card 1</p>
+              <p>Question Card 1</p>
+          </div>
+
       </>
   );
 }
