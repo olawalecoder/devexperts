66838ed8245f2b7ea2c01e9f50f3ec0e1d78e1fe
olawalecoder <bolawale81@gmail.com>
2024-11-27 03:31:21 +0100

    Implement, Create and Read user routes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    server: ['pino', 'pino-pretty' ],
+    serverExternalPackages: ["pino", "pino-pretty" ],
     images: {
         remotePatterns: [
             {
