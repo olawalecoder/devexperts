f7e788e83ffd367023c7f30674adcc4d60bef50a
olawalecoder <bolawale81@gmail.com>
2025-06-24 13:38:22 +0200

    Genereate AI Answers

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -1,128 +1,30 @@
 "use client";
 
-import {
-    headingsPlugin,
-    listsPlugin,
-    quotePlugin,
-    thematicBreakPlugin,
-    markdownShortcutPlugin,
-    MDXEditor,
-    type MDXEditorMethods,
-    ConditionalContents,
-    ChangeCodeMirrorLanguage,
-    toolbarPlugin,
-    UndoRedo,
-    Separator,
-    BoldItalicUnderlineToggles,
-    ListsToggle,
-    CreateLink,
-    InsertImage,
-    InsertTable,
-    InsertThematicBreak,
-    InsertCodeBlock,
-    linkPlugin,
-    linkDialogPlugin,
-    tablePlugin,
-    imagePlugin,
-    codeBlockPlugin,
-    codeMirrorPlugin,
-    diffSourcePlugin,
-} from "@mdxeditor/editor";
-import { basicDark } from "cm6-theme-basic-dark";
-import { useTheme } from "next-themes";
-import type { ForwardedRef } from "react";
+import { type MDXEditorMethods, type MDXEditorProps } from "@mdxeditor/editor";
+import dynamic from "next/dynamic";
+import { forwardRef } from "react";
 
-import "@mdxeditor/editor/style.css";
-import "./dark-editor.css";
+type EditorProps = {
+  value: string;
+  fieldChange: (value: string) => void;
+} & Omit<MDXEditorProps, "markdown" | "onChange">;
 
-interface Props {
-    value: string;
-    fieldChange: (value: string) => void;
-    editorRef: ForwardedRef<MDXEditorMethods> | null;
-}
+const InitializedEditor = dynamic(() => import("./InitializedMDXEditor"), {
+  // Make sure we turn SSR off
+  ssr: false,
+});
 
-const Editor = ({ value, editorRef, fieldChange, ...props }: Props) => {
-    const { resolvedTheme } = useTheme();
+const Editor = forwardRef<MDXEditorMethods, EditorProps>(
+  ({ value, fieldChange, ...props }, ref) => (
+    <InitializedEditor
+      {...props}
+      markdown={value}
+      fieldChange={fieldChange}
+      editorRef={ref}
+    />
+  )
+);
 
-    const theme = resolvedTheme === "dark" ? [basicDark] : [];
-
-    return (
-        <MDXEditor
-            key={resolvedTheme}
-            markdown={value}
-            ref={editorRef}
-            className="background-light800_dark200 light-border-2 markdown-editor dark-editor grid w-full border"
-            onChange={fieldChange}
-            plugins={[
-                headingsPlugin(),
-                listsPlugin(),
-                linkPlugin(),
-                linkDialogPlugin(),
-                quotePlugin(),
-                thematicBreakPlugin(),
-                markdownShortcutPlugin(),
-                tablePlugin(),
-                imagePlugin(),
-                codeBlockPlugin({ defaultCodeBlockLanguage: "" }),
-                codeMirrorPlugin({
-                    codeBlockLanguages: {
-                        css: "css",
-                        txt: "txt",
-                        sql: "sql",
-                        html: "html",
-                        saas: "saas",
-                        scss: "scss",
-                        bash: "bash",
-                        json: "json",
-                        js: "javascript",
-                        ts: "typescript",
-                        "": "unspecified",
-                        tsx: "TypeScript (React)",
-                        jsx: "JavaScript (React)",
-                    },
-                    autoLoadLanguageSupport: true,
-                    codeMirrorExtensions: theme,
-                }),
-                diffSourcePlugin({ viewMode: "rich-text", diffMarkdown: "" }),
-                toolbarPlugin({
-                    toolbarContents: () => (
-                        <ConditionalContents
-                            options={[
-                                {
-                                    when: (editor) => editor?.editorType === "codeblock",
-                                    contents: () => <ChangeCodeMirrorLanguage />,
-                                },
-                                {
-                                    fallback: () => (
-                                        <>
-                                            <UndoRedo />
-                                            <Separator />
-
-                                            <BoldItalicUnderlineToggles />
-                                            <Separator />
-
-                                            <ListsToggle />
-                                            <Separator />
-
-                                            <CreateLink />
-                                            <InsertImage />
-                                            <Separator />
-
-                                            <InsertTable />
-                                            <InsertThematicBreak />
-
-                                            <InsertCodeBlock />
-                                        </>
-                                    ),
-                                },
-                            ]}
-                        />
-                    ),
-                }),
-            ]}
-            {...props}
-        />
-    );
-};
+Editor.displayName = "Editor";
 
 export default Editor;
